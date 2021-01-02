f5081c8b55a05aeeafcfaa1318753b0693c5111b
damian-horna <damian.horna@interia.pl>
2021-01-02 02:24:40 +0100

    Add some comments

--- src/main/java/cassdemo/Main.java
+++ src/main/java/cassdemo/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	private static final int PLANE_ID = 0;
 	private static final int ROW_NUMBER = 12;
 	private static final int ROW_SIZE = 6;
+	private static final int NUM_OF_CUSTOMERS = 1000;
 
 	public static void main(String[] args) throws IOException, BackendException {
 		String contactPoint = null;
@@ -32,6 +33,12 @@ public class Main {
 		Plane plane = new Plane(session, PLANE_ID, ROW_NUMBER, ROW_SIZE);
 		Hotel hotel = new Hotel(0, "Madrid", 10,10,10,10,10, 10);
 		Flight flight = new Flight(0, plane, "Warsaw", "Madrid");
+
+
+		// Create NUM_OF_CUSTOMERS threads and start fighting for resources. Each customer:
+		// 1. Looks up available seats (or groups of seats)
+		// 2. Books one of the available seats (or groups of seats) and hotel if they want
+		// At the end the app should collect statistics how many customers were successful with reservation and whether we faced some mistakes.
 
 		System.exit(0);
 	}
