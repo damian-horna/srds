17b3d39b701a5e6a94b66c67ae772d217f170ce7
plutasnyy <kamil.plucinski97@gmail.com>
2021-01-03 15:23:11 +0100

    use case 2

--- src/main/java/cassdemo/Customer.java
+++ src/main/java/cassdemo/Customer.java
@@ -2,7 +2,6 @@ package cassdemo;
 
 import cassdemo.domain.Flight;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -20,7 +19,7 @@ public class Customer implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep((long)(Math.random() * 2000));
+            Thread.sleep((long) (Math.random() * 2000));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -44,19 +43,30 @@ public class Customer implements Runnable {
         Flight randomFlight = flights.get(new Random().nextInt(flights.size()));
         List<Integer> availableSeats = this.dbService.selectAllAvailableSeatsInFlight(randomFlight.id);
         List<List<Integer>> allPossibleSeats = SeatsManager.findAllPossibleSeatsGroups(availableSeats, 2);
-
-        System.out.println(availableSeats);
-        System.out.println(allPossibleSeats);
-
-        List<Integer> chosenSeats = allPossibleSeats.get(new Random().nextInt(allPossibleSeats.size()));
-        for (Integer seat : chosenSeats) {
-           this.dbService.reserveSeatInFlight(seat, randomFlight, this.id);
+        if (!allPossibleSeats.isEmpty()) {
+            List<Integer> chosenSeats = allPossibleSeats.get(new Random().nextInt(allPossibleSeats.size()));
+            for (Integer seat : chosenSeats) {
+                this.dbService.reserveSeatInFlight(seat, randomFlight, this.id);
+            }
+            System.out.println("Customer " + this.id + " doing use case 1, reserve: " + chosenSeats + " in flight: " + randomFlight);
+        } else {
+            System.out.println("Customer " + this.id + " didn't find seats in flight: " + randomFlight);
         }
-        System.out.println("Customer " + this.id + " doing use case 1, reserve: " + chosenSeats + " in flight: " + randomFlight);
     }
 
     public void useCase2() {
         System.out.printf("Customer %d doing use case 2\n", this.id);
+        List<Flight> flights = this.dbService.selectAllFlights();
+        Flight randomFlight = flights.get(new Random().nextInt(flights.size()));
+        List<Integer> availableSeats = this.dbService.selectAllAvailableSeatsInFlight(randomFlight.id);
+        List<List<Integer>> allPossibleSeats = SeatsManager.findAllPossibleSeatsGroups(availableSeats, 1);
+        if (!allPossibleSeats.isEmpty()) {
+            List<Integer> chosenSeat = allPossibleSeats.get(new Random().nextInt(allPossibleSeats.size()));
+            this.dbService.reserveSeatInFlight(chosenSeat.get(0), randomFlight, this.id);
+            System.out.println("Customer " + this.id + " doing use case 2, reserve: " + chosenSeat + " in flight: " + randomFlight);
+        } else {
+            System.out.println("Customer " + this.id + " didn't find a seat in flight: " + randomFlight);
+        }
     }
 
     public void useCase3() {
